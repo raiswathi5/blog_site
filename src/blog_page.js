ce96091e8697ae9c8a84d7a5af8da7b8952e1d4c
Swathi Rai <swathi.rai@resson.com>
2020-01-22 17:22:44 +0530

    Basic structure

--- src/blog_page.js
+++ src/blog_page.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import './blog_page.css';
+
 class BlogPage extends React.Component{
   constructor() {
     super();
@@ -20,15 +22,23 @@ class BlogPage extends React.Component{
 
 render() {
     return (
-      <div>
+      <div className="row">
         { this.state.blogs.map(blog =>
-            <div>
-                <img src={blog.imgs.large} alt="image" />
+            <div className="leftcolumn">
+              <div class="card">
+                <h2>{blog.title}</h2>
+                <h5>{blog.category}, {blog.author}</h5>
+                <img src={blog.imgs.large} alt="image" height="200px" />
+                {/* <div class="fakeimg" style="height:200px;">Image</div> */}
+                <p>{blog.description}</p>
+              </div>
+
+                {/* <img src={blog.imgs.large} alt="image" />
                 <div>Title: {blog.title}</div>
                 <div>Author: {blog.author}</div>
                 <div>Description: {blog.description}</div>
                 <div>Category: {blog.category}</div>
-                <img src={blog.imgs.thumb} alt="image" height="42" width="42"/>
+                <img src={blog.imgs.thumb} alt="image" height="42" width="42"/> */}
             </div>
         )}
       </div>
